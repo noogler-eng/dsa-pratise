d3f879dcb4891f2bc335787240e34073b311c91b
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-13 17:30:36 +0530

    add

---
+++ neetcode/binarySearch/1.cpp
@@ -0,0 +1,20 @@
+// QUESTION - https://leetcode.com/problems/search-a-2d-matrix/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// time complexity O(m + n)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // here there is no sqare matrx so remember
+        int row = 0, col = matrix[0].size()-1;
+        while(row < matrix.size() && col > -1){
+            if(matrix[row][col] == target) return true;
+            else if (target > matrix[row][col]) row++;
+            else col--;
+        }
+        return false;
+    }
+};
