1219daf9abe4c410a09c07154da7d4a9d6b09f85
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-27 21:53:42 +0530

    add

--- chapters/chapter-1/14_sort_linked_list.cpp
+++ chapters/chapter-1/14_sort_linked_list.cpp
@@ -41,4 +41,93 @@ public:
     }
 };
 
+// this is brute force solution 
+// this solution takes the time complexity o(N + N + NlogN)
+// space complexity o(N)
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> v;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        // basically this is merge sort
+        sort(v.begin(), v.end());
+        temp = head;
+        int i=0;
+        while(temp!=NULL){
+            temp->val = v[i];
+            i++;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+
 // Merge Sort with Time Complexity o(NLogN)
+// always remember the merge sort code snippets
+// void merge_sort(arr, low, high){
+//     if(low == high) return;
+//     int mid = (low + high ) / 2;
+//     merge_sort(arr, low, mid);
+//     merge_sort(arr, mid+1, high);
+//     merge(arr, low, mid, high);
+// }
+class Solution {
+public:
+    ListNode* getMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    // this is how we merger two sorted linkedlist
+    ListNode* merge(ListNode* leftHead, ListNode* rightHead) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while (leftHead != NULL && rightHead != NULL) {
+            if (leftHead->val < rightHead->val) {
+                temp->next = leftHead;
+                leftHead = leftHead->next;
+            } else {
+                temp->next = rightHead;
+                rightHead = rightHead->next;
+            }
+            temp = temp->next;
+        }
+
+        if (leftHead != NULL) {
+            temp->next = leftHead;
+        }
+
+        if (rightHead != NULL) {
+            temp->next = rightHead;
+        }
+
+        return dummy->next;
+    }
+
+    ListNode* merge_sort(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* middle_node = getMiddle(head);
+        ListNode* leftHead = head;
+        ListNode* rightHead = middle_node->next;
+        middle_node->next = NULL;
+
+        leftHead = merge_sort(leftHead);
+        rightHead = merge_sort(rightHead);
+
+        return merge(leftHead, rightHead);
+    }
+
+    ListNode* sortList(ListNode* head) { return merge_sort(head); }
+};
