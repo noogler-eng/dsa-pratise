04514348f4247f923a8657fa2a0f745b52a89cd8
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-30 12:46:32 +0530

    add

--- chapters/chapter-8/4_shortest_distance_between_two_nodes.cpp
+++ chapters/chapter-8/4_shortest_distance_between_two_nodes.cpp
@@ -21,7 +21,9 @@ struct TreeNode {
 // 'U' means to go from a node to its parent node.
 
 // step1. finding the lowest parent of them
-// step2. 
+// step2. travese them on the both side like travelling from right as well as from left
+// here we are using the true and false wheather to find that this direction contains node or not
+// if not then simpley pop, if found then return not poping
 class Solution {
 public:
 
@@ -36,10 +38,23 @@ public:
         else return root;
     }
 
+    bool toLCA(TreeNode* root, int target, string& path){
+        if (root == NULL) return false;
+        if (root->val == target) return true;
+        path.push_back('U');
+        if (toLCA(root->left, target, path) || toLCA(root->right, target, path)) {
+            return true;
+        }
+
+        path.pop_back();
+        return false;
+    }
+
+
     bool findPath(TreeNode* root, int target, string& path, char direction) {
         if (!root) return false;
-        if (root->val == target) return true;
         path.push_back(direction);
+        if (root->val == target) return true;
         if (findPath(root->left, target, path, 'L') || findPath(root->right, target, path, 'R')) {
             return true;
         }
@@ -49,10 +64,29 @@ public:
 
     string getDirections(TreeNode* root, int startValue, int destValue) {
         TreeNode* lowest_common_parent = helper(root, startValue, destValue);
-        string ans="";
+        string path1="";
+        string path2="";
+        toLCA(lowest_common_parent, startValue, path1);
+        findPath(lowest_common_parent, destValue, path2, '.');
+        cout<<path1<<" "<<path2<<endl;
 
-        findPath(root, startValue, ans, 'U');
-        findPath(root, destValue, ans, '\0');
-        return ans;
+        return path1+path2.substr(1, path2.length());
     }
 };
+
+// we can aslo do this
+// after this place all the L, R to U in upper string
+string upper, lower;
+void findingPath(TreeNode* root, int startValue, int destValue, string path){
+    if(root == NULL) return;
+    if(root->val == startValue) upper = path;
+    if(root->val == destValue) lower = path;
+
+    path.push_back('L');
+    findingPath(root->left, startValue, destValue, path);
+    path.pop_back();
+
+    path.push_back('R');
+    findingPath(root->right, startValue, destValue, path);
+    path.pop_back();
+}
